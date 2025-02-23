46151e17177f1d3eb94576df035780aa6cf1b076
Aditya-Sakpal <aditya.as@somaiya.edu>
2025-02-24 04:01:23 +0530

    resovled few bugs

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers.query_router import router as query_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 @app.get("/")
 def read_root():
--- routers/query_router.py
+++ routers/query_router.py
@@ -1,20 +1,20 @@
 import traceback
-import json 
+import json
 
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
-from schemas.query_schema import QueryRequest,QueryResponse
-from utils.logger import log_performance , logger
+from schemas.query_schema import QueryRequest, QueryResponse
+from utils.logger import log_performance, logger
 from utils.openai_funcs import get_openai_response
-from utils.constants import QUERY_CLASSIFICATION_USER_PROMPT, QUERY_CLASSIFICATION_SYSTEM_PROMPT , GREET_USER_PROMPT , GREET_SYSTEM_PROMPT \
-    , RESPONSE_GENERATION_USER_PROMPT , RESPONSE_GENERATION_SYSTEM_PROMPT
+from utils.constants import QUERY_CLASSIFICATION_USER_PROMPT, QUERY_CLASSIFICATION_SYSTEM_PROMPT, GREET_USER_PROMPT, GREET_SYSTEM_PROMPT, RESPONSE_GENERATION_USER_PROMPT, RESPONSE_GENERATION_SYSTEM_PROMPT
 from utils.pinecone_funcs import retrieve_chunks
 
 router = APIRouter()
 
+
 @log_performance
-@router.post("/query",response_model=QueryResponse)
+@router.post("/query", response_model=QueryResponse)
 async def query(request: QueryRequest):
     try:
         type = get_openai_response(
@@ -26,56 +26,60 @@ async def query(request: QueryRequest):
                 {
                     "role": "user",
                     "content": QUERY_CLASSIFICATION_USER_PROMPT.format(query=request.query)
-                }    
+                }
             ],
             is_json=True
         )
         type = json.loads(type)['type']
-        
+
         if type == "garbage":
-            response =  "I guess you are typed some gibberish. Please type a valid query."
-        
-        elif type == "greet":  
-            messages = request.messages.append(
+            response = "I guess you are typed some gibberish. Please type a valid query."
+
+        elif type == "greet":
+            request.messages.append(
                 {
                     "role": "system",
                     "content": GREET_SYSTEM_PROMPT
-                },
+                }
+            )
+            request.messages.append(
                 {
                     "role": "user",
                     "content": GREET_USER_PROMPT.format(query=request.query)
                 }
             )
             response = get_openai_response(
-                messages = messages,
-                is_json = False
+                messages=request.messages,
+                is_json=False
             )
         else:
             chunks = retrieve_chunks(
-                        namespace = "chatbot",
-                        query = request.query
-                    )
-            
+                namespace="chatbot",
+                query=request.query
+            )
+
             context = '\n'.join(chunks) if chunks is not None else ""
-            
-            messages = request.messages.append(
+
+            request.messages.append(
                 {
                     "role": "system",
                     "content": RESPONSE_GENERATION_SYSTEM_PROMPT
-                },
+                }
+            )
+            request.messages.append(
                 {
                     "role": "user",
                     "content": RESPONSE_GENERATION_USER_PROMPT.format(
-                        context = context,
-                        query = request.query
+                        context=context,
+                        query=request.query
                     )
                 }
-            )   
+            )
             response = get_openai_response(
-                messages = messages,
-                is_json = False
-            )    
-        
+                messages=request.messages,
+                is_json=False
+            )
+
         return JSONResponse(content={"message": response})
     except Exception as e:
         logger.error(f"Error in query router: {traceback.format_exc()}")
